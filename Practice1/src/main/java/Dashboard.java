5bd648ae45dca68aaf4075acd593e1aca42ef390
Nisha <Nisha@DESKTOP-G6T6D2K>
2022-02-08 12:02:12 +0530

    First Commit

---
+++ Practice1/src/main/java/Dashboard.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Dashboard
+ */
+@WebServlet("/Dashboard")
+public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Dashboard() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session=request.getSession(false);  
+        String userId = null;
+        if (session.getAttribute("userid") != null)
+            userId =(String)session.getAttribute("userid");  
+        if (userId == null ) {
+            System.out.println("No UserId was found in session.<br>");
+        } else {
+            System.out.println("UserId obtained from session :" + userId + "<br>");
+            System.out.println("<a href='logout'>Logout of session</a><br>");
+        }
+        System.out.println("</body></html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
